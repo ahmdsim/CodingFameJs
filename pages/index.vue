faa42f01ec12ba17069e853b589c1a453d259fde
Sergei Gorlanov <s.gorlanov@smartinmedia.com>
2022-03-09 13:31:38 +0100

    reverted fix

--- pages/index.vue
+++ pages/index.vue
@@ -65,6 +65,7 @@
               </v-col>
               <v-col cols="3">
                 <v-btn
+                  v-if="index !== repos.length - 1"
                   class="mx-2"
                   fab
                   dark
