7d8720a774ef54fb04f02c6b2a20996ed58f1170
Sergey <gorlanov.s@gmail.com>
2023-01-27 21:08:20 +0100

    fixed config

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,7 @@ export default {
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'http://http://10.16.18.17/'
+    baseURL: process.env.NODE_ENV !== 'production' ? 'http://localhost:3000' : 'http://10.16.18.17/'
   },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
